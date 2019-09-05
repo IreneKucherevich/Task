23205bbb92c37891285bd222e17bc5cf6197771c
Ira Kucherevich <irina.kucherevich@yandex.ru>
2019-09-05 22:50:59 +0300

    Created navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { UsersComponent } from './users/users.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    UsersComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    { path: 'users', component: UsersComponent},
+    { path: 'tasks', component: TasksComponent }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
